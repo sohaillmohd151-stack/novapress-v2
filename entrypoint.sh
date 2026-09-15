558e1316871548895046fdd637107c05ccd18536
sohaillmohd151-stack <sohaillmohd151@gamil.com>
2026-09-15 18:59:55 +0300

    Fix permissions dynamically in entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+# Fix storage and cache permissions dynamically at runtime
+chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
+chmod -R 775 /var/www/html/storage /var/www/html/bootstrap/cache
+
 php artisan config:cache
 php artisan route:cache
 php artisan migrate --force
